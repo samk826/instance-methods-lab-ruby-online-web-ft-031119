0527b3fabe8f26f7a46adcc87911d3d0900c379b
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-22 05:35:21 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+
+def talk
+    puts "Hello World!"
+  end
+end
+
+hi= Person.new
+hi.talk
+
+class Person
+
+
+def walk
+    puts "The Person is walking"
+  end
+end
+
+hi= Person.new
+hi.walk
